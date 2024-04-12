f8c636babc96005bca9c53949de9c3f492e49be3
Denys Bazyliak <franciskdelyamon@gmail.com>
2024-04-12 14:02:39 +0200

    feat: change deprecated img to Image

--- src/layout/MainLayout/Drawer/CollapsedDrawer.tsx
+++ src/layout/MainLayout/Drawer/CollapsedDrawer.tsx
@@ -2,6 +2,7 @@
 // Libraries
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 // Mui Icons
@@ -11,7 +12,7 @@ import ReplyIcon from '@mui/icons-material/Reply';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 // Internal Imports
-
+import Dictionary from '../../../../public/images/book.png'
 // Types
 // type CollapsedDrawerProps = {
 //     expand: boolean,
@@ -21,7 +22,7 @@ function CollapsedDrawer() {
     return (
         <nav className={`fixed top-0 left-0 h-screen  w-[60px] mr-[60px] flex flex-col bg-white gap-y-2 border-r-[1px] `}>
             <div className={'relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto cursor-pointer'}>
-                <img src={'https://cdn-icons-png.flaticon.com/128/3627/3627782.png'} alt={'😍'} />
+            <Image src={Dictionary} width={300} height={300} alt={'dictionary'} />
             </div>
             <div className={`flex flex-col justify-space-around items-center h-[calc(100vh-200px)]`}>
 
--- src/layout/MainLayout/Drawer/ExpandedDrawer.tsx
+++ src/layout/MainLayout/Drawer/ExpandedDrawer.tsx
@@ -2,6 +2,7 @@
 // Libraries
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image'
 
 
 // Mui Icons
@@ -11,6 +12,7 @@ import ReplyIcon from '@mui/icons-material/Reply';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 // Internal Imports
+import Dictionary from '../../../../public/images/book.png'
 
 // Types
 // type ExpandedDrawerProps = {
@@ -21,11 +23,11 @@ function ExpandedDrawer() {
     return (
         <nav className={`fixed top-0 left-0 h-screen w-[200px] mr-[60px] flex flex-col bg-white gap-y-2 border-r-[1px] `}>
             <div className={'relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto cursor-pointer'}>
-                <img src={'https://cdn-icons-png.flaticon.com/128/3627/3627782.png'} alt={'😍'} />
+                <Image src={Dictionary} width={300} height={300} alt={'dictionary'} />
             </div>
             <div className={`flex flex-col justify-space-around items-center h-[calc(100vh-200px)]`}>
 
-                <Link href='/dictionary' className={'drawer-button'}>
+                <Link href='/dictionary' className={'drawer-button border-r-4 border-blue-500'}>
                     <div><MenuBookIcon /></div>
                     <span>Home</span>
                 </Link>
