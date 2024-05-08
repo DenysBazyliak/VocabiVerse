00a2d3ea88d430931f191a5e8ef1330c55cd9fd4
Denys Bazyliak <franciskdelyamon@gmail.com>
2024-05-08 16:25:08 +0200

    create: create WordForm carcass

---
+++ src/_components/WordForm.tsx
@@ -0,0 +1,35 @@
+// Library Imports
+import { useForm, SubmitHandler } from 'react-hook-form';
+import React from 'react';
+
+type Inputs = {
+    example: string
+    exampleRequired: string
+}
+type WordFormProps = {
+    form: boolean,
+    setForm: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const WordForm = ({ form, setForm }: WordFormProps) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+    return (
+        <div
+            className={' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] z-60 h-[300px] flex flex-col justify-center items-center bg-[#C5C6C7] rounded-3xl  '}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input defaultValue='test' {...register('example')} />
+                <input {...register('exampleRequired', { required: true })} />
+                {errors.exampleRequired && <span>This field is required</span>}
+                <input type='submit' className={''} />
+            </form>
+        </div>
+    );
+};
+
+export default WordForm;
+  
